9d5549d933a71de36894ddca4f220703bb322b1a
sultanchik1serik <142357734+sultanchik1serik@users.noreply.github.com>
2023-09-22 04:40:49 -0400

    Create code_2.py

---
+++ code_2.py
@@ -0,0 +1,39 @@
+
+    for item in data:
+if __name__ == "__main__":
+
+
+def generate_random_data():
+    data = generate_random_data()
+    data = [random.randint(1, 100) for _ in range(10)]
+    main()
+
+        print(f"Random Number: {item}")
+def main():
+import random
+    return data        int[] data = new int[10];
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Random Number: " + item);
+        for (int item : data) {
+        }
+}
+    public static void main(String[] args) {
+public class RandomDataGenerator {
+            data[i] = (int) (Math.random() * 100) + 1;
+    }
+        }
+
+if __name__ == "__main__":
+    main()
+    data = [random.randint(1, 100) for _ in range(10)]
+def main():
+import random
+
+
+
+    return data
+def generate_random_data():
+
+        print(f"Random Number: {item}")
+    data = generate_random_data()
+    for item in data:
